df8ff5a994174ab2a8cdb3fae7a4881051d46dca
hessm88 <mhess@familysearch.org>
2015-03-10 13:05:52 -0600

    Add functionality for twitter bloodhound typeahead to search bar
    We want the search bar to return results for usernames when
    something is typed into the search bar. This commit uses the
    Twitter Bloodhound JS (integrated with jQuery) to provide this
    functionality.

--- frontend/controllers/UserController.php
+++ frontend/controllers/UserController.php
@@ -3,9 +3,12 @@ namespace frontend\controllers;
 
 use common\models\User;
 use DateTime;
+use frontend\models\db\dao\UserDao;
 use frontend\models\db\record\ItemRequest;
 use frontend\models\db\record\ItemRequestComment;
 use frontend\models\ItemRequestUtil;
+use mysqli;
+use Yii;
 use yii\base\Controller;
 use yii\base\Response;
 use yii\filters\AccessControl;
@@ -52,6 +55,59 @@ class UserController extends Controller {
                 'class' => 'yii\web\ErrorAction',
             ],
         ];
+    }
+
+/*    public function actionGetUsers() {
+        $db = \Yii::$app->db;
+        $queryParam = \Yii::$app->getRequest()->getQueryParam("query");
+
+        $query = $db->createCommand("SELECT username FROM user " .
+            " WHERE username LIKE :query;");
+        $results = $query->bindValue(":query", "%" . $queryParam . "%")->queryAll();
+
+        \Yii::$app->response->format = 'json';
+        return $results;
+    }*/
+
+    public function actionGetUsers() {
+        $db = Yii::$app->db;
+        \Yii::$app->response->format = 'json';
+        $queryParam = \Yii::$app->getRequest()->getQueryParam("query");
+
+        // query with mysqli for better performance
+        $dbName = substr($db->dsn, strpos($db->dsn, "dbname") + 7);
+        $conn = new mysqli("localhost", $db->username, $db->password, $dbName);
+        // Check connection
+        if ($conn->connect_error) {
+            //die("Connection failed: " . $conn->connect_error);
+            // ERRRRRRRRRRRRRRRRRRORRRRRRRRRRR!
+        }
+
+        $sql = "SELECT username FROM user WHERE username LIKE '%" .
+            $conn->real_escape_string($queryParam) .  "%';";
+        $result = $conn->query($sql);
+        if ($conn->connect_error) {
+            // log the error
+            //echo $conn->connect_error;
+            return array();
+        }
+        if ((!$result) || ($result->num_rows < 1)) {
+            return array();
+        }
+
+        // store parentId's in an array
+        $usernames = array();
+        while ($row = $result->fetch_assoc()) {
+            $username = $row["username"];
+            if (!in_array($username, $usernames)) {
+                $usernames[] = ["username" => $username];
+            }
+        }
+
+        // free the result set
+        $result->free();
+
+        return $usernames;
     }
 
     public function actionDirectory() {
--- frontend/web/js/User.js
+++ frontend/web/js/User.js
@@ -1,47 +1,38 @@
 $(document).ready(function() {
-  var substringMatcher = function (strs) {
-    return function findMatches(q, cb) {
-      var matches, substrRegex;
+  var movies = new Bloodhound({
+    datumTokenizer: function (datum) {
+      return Bloodhound.tokenizers.whitespace(datum.value);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: 'http://localhost/public_html/mr-test-two/frontend/web/index.php?r=user%2Fget-users&query=%QUERY',
+      maxParallelRequests: 1,
+      ajax: {
+        type: "GET",
+        timeout: 10000
+      },
+      triggerLength: 3,
+      filter: function (users) {
+        return $.map(users, function(usernameArray) {
+          return {value: usernameArray.username}
+        })
+      }
+    }
+  });
 
-      // an array that will be populated with substring matches
-      matches = [];
+  movies.initialize();
 
-      // regex used to determine if a string contains the substring `q`
-      substrRegex = new RegExp(q, 'i');
-
-      // iterate through the pool of strings and for any string that
-      // contains the substring `q`, add it to the `matches` array
-      $.each(strs, function (i, str) {
-        if (substrRegex.test(str)) {
-          // the typeahead jQuery plugin expects suggestions to a
-          // JavaScript object, refer to typeahead docs for more info
-          matches.push({value: str});
-        }
-      });
-
-      cb(matches);
-    };
-  };
-
-  var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-  ];
-
-  $('#the-basics .typeahead').typeahead({
-                                          hint: true,
-                                          highlight: true,
-                                          minLength: 1
-                                        },
-                                        {
-                                          name: 'states',
-                                          displayKey: 'value',
-                                          source: substringMatcher(states)
-                                        });
+  $('#the-basics .typeahead').typeahead(null, {
+    displayKey: 'value',
+    source: movies.ttAdapter(),
+    templates: {
+      empty: ['<div class="empty-message">',
+        'unable to find any users',
+        '</div>'
+        ].join('\n'),
+      suggestion: function (value) {
+        return '<p class="repo-language"><b>' + value.value + '</b></p>'
+      }
+    }
+  });
 });
