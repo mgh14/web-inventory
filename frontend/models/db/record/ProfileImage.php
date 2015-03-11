f67a16fceec6579b9c198ebe70696adfec25e91a
hessm88 <mhess@familysearch.org>
2015-03-10 22:59:29 -0600

    Add method to profile image record to get records by user ID

--- frontend/models/db/record/ProfileImage.php
+++ frontend/models/db/record/ProfileImage.php
@@ -9,4 +9,23 @@ class ProfileImage extends ActiveRecord {
         return "profile_image";
     }
 
+    public static function getByUserIds($userIds, $useIdAsKey = false) {
+        $query = (new \yii\db\Query())->select(['id', 'filename'])->
+            from(static::tableName())->where(array('in', 'user_id', $userIds));
+        $command = $query->createCommand();
+        $data = $command->queryAll();
+
+        $images = array();
+        foreach($data as $result) {
+            if ($useIdAsKey) {
+                $images[$result['id']] = $result['filename'];
+            }
+            else {
+                $images[] = $result['filename'];
+            }
+        }
+
+        return $images;
+    }
+
 }
