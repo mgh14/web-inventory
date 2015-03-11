ac310bf9ab1a9abacdc8e05acee909c472f985b7
hessm88 <mhess@familysearch.org>
2015-03-10 23:02:27 -0600

    Add search button and 'loading' gif to the user directory view

--- frontend/views/user/user-directory.php
+++ frontend/views/user/user-directory.php
@@ -2,7 +2,7 @@
 use frontend\assets\UserAsset;
 use yii\web\View;
 
-/**
+/** (no vars)
  */
 
 $this->title = "User Directory";
@@ -11,7 +11,19 @@ $this->title = "User Directory";
 UserAsset::register($this, View::POS_BEGIN);
 ?>
 
-<div id="the-basics">
-    <input class="typeahead" type="text" placeholder="States of USA">
+<div id="the-basics" class="floatLeft">
+    <input id="searchBar" class="typeahead" type="text" placeholder="States of USA">
 </div>
+<div id='loader1' class="floatLeft loader">
+    <img src="http://localhost/public_html/mr-test-two/frontend/images/load/ajax-loader.gif"/>
+</div>
+<button class="floatLeft" id="searchUsersBtn">Search</button>
+
+<div id="users"></div>
+
+<style>
+    .loader {
+        display: none;
+    }
+</style>
 
