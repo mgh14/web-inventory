bab70aa03ae1f4ac49490e41657bcd890fcad5df
hessm88 <mhess@familysearch.org>
2015-03-16 12:27:01 -0600

    Fix up profile view html a little bit

--- frontend/views/profile/profile.php
+++ frontend/views/profile/profile.php
@@ -14,19 +14,23 @@ $this->title = ucfirst($user->username) . "'s Profile";
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
 
-<div class="">
-    <div>
+<div style="display: inline-block; width: 100%;">
+    <div style="display: inline-block;">
         <?php echo $profileImgHtml?>
-        <h1 class="floatLeft"><?= Html::encode($this->title) ?></h1>
     </div>
 
+    <div class="spacer" style="width:8%; display: inline-block; height:1px;"></div>
+
+    <div class="" style="display: inline-block; vertical-align: top;">
+        <h1 class=""><?= Html::encode($this->title) ?></h1>
+        <br/>
+        <div class="">
+            <p>Name: <?php echo ucfirst($user['first_name']) . " " .
+                    ucfirst($user['last_name'])?></p>
+            <p>Email: <?php echo $user->email?></p>
+            <p>Member Type: <?php echo $accountType?></p>
+            <p>Member since: <?php echo date("F j, Y")?></p>
+        </div>
+    </div>
     <div style="clear: both;"></div>
-
-    <br/><br/><br/><br/>
-
-    <div>
-        <p>Email: <?php echo $user->email?></p>
-        <p>Member Type: <?php echo $accountType?></p>
-        <p>Member since: <?php echo date("F j, Y")?></p>
-    </div>
 </div>
