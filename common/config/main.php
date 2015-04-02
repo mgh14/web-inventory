509c069ff5aef97ee26d9cc3763b5125b5104e73
mgh14 <chasing314@gmail.com>
2015-04-01 21:21:43 -0600

    Add asset manager experiment
    Experimenting with asset manager stuff. May be useful to
    have later.

--- common/config/main.php
+++ common/config/main.php
@@ -8,5 +8,15 @@ return [
         'authManager' => [
             'class' => 'yii\rbac\DbManager',
         ],
+        'assetManager' => [
+            /*'bundles' => [
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [],
+                ],
+            ],*/
+            /*'assetMap' => [
+                'jquery-ui.min.css' => "https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.min.css"
+            ],*/
+        ],
     ],
 ];
